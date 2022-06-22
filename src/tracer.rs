4f0d36bc704641df0861ed47bbef66b77e421441
Maoyu <maoyu.wang@mail.mcgill.ca>
2022-06-22 11:52:49 -0400

    update log method

--- src/tracer.rs
+++ src/tracer.rs
@@ -11,7 +11,11 @@ pub struct Log<E: Display + Clone> {
 impl<E: Display + Clone> Display for Log<E> {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Error {}, BackTrace\n {:?}", self.error, self.backtrace)
+        write!(
+            f,
+            "Error: {} \nBacktrace:\n{:?}",
+            self.error, self.backtrace
+        )
     }
 }
 
@@ -40,13 +44,39 @@ impl<A, E: Display + Clone> Tracer for Result<A, E> {
     }
 }
 
+pub trait Logger {
+    type Log;
+
+    fn get_log(&self) -> Option<Self::Log>;
+    fn print_log(&self);
+}
+
+impl<A, E: Display + Clone> Logger for Result<A, Log<E>> {
+    type Log = Log<E>;
+
+    #[inline]
+    fn get_log(&self) -> Option<Self::Log> {
+        match *self {
+            Ok(_) => None,
+            Err(ref l) => Some(l.clone()),
+        }
+    }
+
+    #[inline]
+    fn print_log(&self) {
+        if let Err(ref l) = *self {
+            println!("{}", l);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::Tracer;
+    use super::{Logger, Tracer};
 
     #[test]
     fn it_works() {
         let x: Result<i32, i32> = Err(2);
-        println!("{:?}", x.log());
+        x.log().print_log();
     }
 }
